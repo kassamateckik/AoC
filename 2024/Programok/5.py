d985141049e648ea73c183ab21e533654b14f29b
kassa.mate <kassa.mate@ckik.hu>
2024-12-05 10:30:29 +0100

    2024/5/2

--- 2024/Programok/5.py
+++ 2024/Programok/5.py
@@ -36,3 +36,25 @@ for i in range(len(oldalak)):
 
 # print(s)
 
+def csere(l, i ,j):
+    a = l[i]
+    l[i] = l[j]
+    l[j] = a
+
+
+for i in range(len(rosszak)):
+    for j in range(len(szabalyok)):
+        a = szabalyok[j].split("|")[0]
+        b = szabalyok[j].split("|")[1]
+        for k in range(len(rosszak[i])):
+            for h in range(k, len(rosszak[i])):
+                if rosszak[i][k] == a and rosszak[i][h] == b and h <= k:
+                    csere(rosszak[i], k, h)
+                    
+s = 0 
+for i in range(len(rosszak)):
+    s += int(rosszak[i][int((len(rosszak[i])-1)/2)])
+
+# 4937 - low
+# 5013 - high
+print(s)
